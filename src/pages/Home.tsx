ac75b703a029dadb3f6eb2a7aa4d160c36348fbf
jeron7 <jeronimojairoo@gmail.com>
2021-06-24 20:18:38 -0300

    Code of the third lesson

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,46 @@
+import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom'
 
-import { useAuth } from '../hooks/useAuth'
+import { database } from '../services/firebase';
+import { useAuth } from '../hooks/useAuth';
 
-import { Button } from '../components/Button'
+import { Button } from '../components/Button';
 import { AsideIllustration } from '../components/AsideIllustration';
 
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg'
-import enterIcon from '../assets/images/enter.svg'
+import googleIconImg from '../assets/images/google-icon.svg';
+import enterIcon from '../assets/images/enter.svg';
 
-import '../styles/auth.scss'
+import '../styles/auth.scss';
 
 export function Home() {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState('');
 
   async function handleCreateRoom() {
     if (!user) {
       await signInWithGoogle();
     }
     
-    history.push('/rooms/new')
+    history.push('/rooms/new');
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists.');
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
   }
 
   return (
@@ -34,8 +54,12 @@ export function Home() {
             Crie sua sala com o Google
           </button>
           <div className="separator">ou entre em uma sala</div>
-          <form>
-            <input type="text" placeholder="Digite o código da sala" />
+          <form onSubmit={handleJoinRoom}>
+            <input 
+              type="text" 
+              placeholder="Digite o código da sala"
+              onChange={ event => setRoomCode(event.target.value) }
+            />
             <Button type="submit">
               <img src={ enterIcon } alt="Entrar na sala"/>
               Entrar na sala
